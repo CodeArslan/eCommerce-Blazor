c9c91af593442e6d7c4779f535b1c354f39c7853
arslan5423 <sayhi.arslan>
2024-12-18 09:45:18 +0500

    Toastr Notification Added

---
+++ eCommerce/Services/Extensions/IJSRuntimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace eCommerce.Services.Extensions
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async Task ToastrSuccess(this IJSRuntime _JS,string message)
+        {
+            await _JS.InvokeVoidAsync("ShowToastr","Success",message);
+        }
+        public static async Task ToastrError(this IJSRuntime _JS, string message)
+        {
+            await _JS.InvokeVoidAsync("ShowToastr", "Error", message);
+        }
+    }
+}
---
+++ eCommerce/wwwroot/js/Notifications.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = function (type, message) {
+    if (type == "Success") {
+        toastr.success(message);
+    }
+    if (type == "Error") {
+        toastr.error(message);
+    }
+}
