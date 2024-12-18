1277ef659da79cff59935fba3f989a7d6aa70a4d
arslan5423 <sayhi.arslan>
2024-12-18 10:50:25 +0500

    Delete categroy functionality added

---
+++ eCommerce/wwwroot/js/Common.js
@@ -0,0 +1,7 @@
+﻿function ShowConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('ConfirmationModal')).show();
+}
+
+function HideConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('ConfirmationModal')).hide();
+}
