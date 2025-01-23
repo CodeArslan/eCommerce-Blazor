0f54563b164777a9989b2e70b12034e1b9ff1d1d
arslan5423 <sayhi.arslan>
2025-01-23 08:52:41 +0500

    Order Placement Imeplemented

--- eCommerce/Utility/StaticDetails.cs
+++ eCommerce/Utility/StaticDetails.cs
@@ -1,4 +1,6 @@
-﻿namespace eCommerce.Utility
+﻿using eCommerce.Data;
+
+namespace eCommerce.Utility
 {
     public static class StaticDetails
     {
@@ -9,5 +11,24 @@
         public static string StatusReadyForPickup = "ReadyForPickup";
         public static string StatusCompleted = "Completed";
         public static string StatusCancelled = "Cancelled";
+
+
+        public static List<OrderDetails> ConvertShoppinCartListToOrderDetailList(List<eCommerce.Data.Cart> shoppingCartList)
+        {
+            List<OrderDetails> orderDetailsList = new List<OrderDetails>();
+            foreach (var cart in shoppingCartList)
+            {
+                OrderDetails orderDetails = new OrderDetails
+                {
+                    ProductId = cart.ProductId,
+                    Product = cart.Product,
+                    ProductName=cart.Product.Name,
+                    Price = Convert.ToDouble(cart.Product.Price),
+                    Count = cart.Count
+                };
+                orderDetailsList.Add(orderDetails);
+            }
+            return orderDetailsList;
+        }
     }
 }
