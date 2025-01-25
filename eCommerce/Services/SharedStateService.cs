db4c5f92ee639abe98c336fddd07f7a71011af6f
arslan5423 <sayhi.arslan>
2025-01-25 22:00:12 +0500

    Cart Counter Implemented

---
+++ eCommerce/Services/SharedStateService.cs
@@ -0,0 +1,18 @@
+﻿namespace eCommerce.Services
+{
+    public class SharedStateService
+    {
+        public event Action OnChange;
+        public int _TotalCartCount;
+        public int TotalCartCount
+        {
+            get { return _TotalCartCount; }
+            set
+            {
+                _TotalCartCount = value;
+                NotifyStateChanged();
+            }
+        }
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
